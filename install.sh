c32b899eb56061c801896e51212a07fbb2f63a5e
wujia <wujia@chinamobile.com>
2023-08-22 11:26:59 +0800

    add nginx-ingress

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+docker pull registry.aliyuncs.com/google_containers/nginx-ingress-controller:v1.8.1@sha256:e5c4824e7375fcf2a393e1c03c293b69759af37a9ca6abdb91b13d78a93da8bd
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-webhook-certgen:v20230407@sha256:543c40fd093964bc9ab509d3e791f9989963021f1e9e4c9c7b6700b02bfb227b
+
+# Install ingress-nginx
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm upgrade --cleanup-on-fail --install --debug ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace --values .\nginx-ingress.yaml
+
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+
+
 # install MINIO
 kubectl create -f minio.yaml
 helm upgrade --install minio-server .\minio\ --namespace minio --create-namespace --values .\minio-config.yaml
